d1a4d5ebda9bc87ff7684413d30ba5cc0e7c053e
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-02-02 16:02:41 +0200

    Change GC preventing command to SDL_CreateWindow

--- exampleClass.cpp
+++ exampleClass.cpp
@@ -11,5 +11,6 @@ void Example::testMethod()
 
   // This line doesn't let emscripten release Module's occupied memory.
   // Comment it out to let the memory be released with Module object destruction.
-  SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 300,
+                     SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 }
