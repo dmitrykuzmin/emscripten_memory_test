e956797308f20f20a90e1d1eae0f829cd6372f17
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-02-02 15:57:44 +0200

    Change GC preventing command to SDL_Quit

--- exampleClass.cpp
+++ exampleClass.cpp
@@ -11,5 +11,5 @@ void Example::testMethod()
 
   // This line doesn't let emscripten release Module's occupied memory.
   // Comment it out to let the memory be released with Module object destruction.
-  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Quit();
 }
